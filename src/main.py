5ef020c9e8de0fbde6cc47d373ff8edaa894301b
donaldnguyen99 <donaldnguyen99@users.noreply.github.com>
2025-01-10 16:40:40 -0800

    update copy function with logging

--- src/main.py
+++ src/main.py
@@ -3,22 +3,20 @@ import os, shutil
 from textnode import TextNode, TextType
 from util import extract_title, markdown_to_html_node
 
-def copy_source_destination(source="static", destination="public"):
+def copy_source_destination(source="static", destination="public", verbose=False):
+    if verbose: print(f"Copying {source} to {destination}")
     if os.path.exists(destination):
         shutil.rmtree(destination)
     os.mkdir(destination)
-
-    items = os.listdir(source)
-    if not items:
-        return
     
-    for item in items:
+    for item in os.listdir(source):
         item_path_source = os.path.join(source, item)
         item_path_destination = os.path.join(destination, item)
         if os.path.isfile(item_path_source):
+            if verbose: print(f"Copying {item_path_source} to {item_path_destination}")
             shutil.copy(item_path_source, item_path_destination)
         else:
-            copy_source_destination(item_path_source, item_path_destination)
+            copy_source_destination(item_path_source, item_path_destination, verbose)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -38,7 +36,8 @@ def main():
     print(TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev"))
     copy_source_destination(
         os.path.join(os.path.curdir, "static"), 
-        os.path.join(os.path.curdir, "public")
+        os.path.join(os.path.curdir, "public"),
+        verbose=True
     )
     generate_page("content/index.md", "template.html", "public/index.html")
     
